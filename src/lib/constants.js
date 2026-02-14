5fac39a6c774e2d0cddd05cc3118d2a15a5db548
SunnyDev7 <sunnydev4589@gmail.com>
2026-02-14 12:31:17 +0100

    pomodoro page ready

---
+++ src/lib/constants.js
@@ -0,0 +1,9 @@
+export const TIME_PRESETS = [
+  { label: "15 min", minutes: 15 },
+  { label: "25 min", minutes: 25 },
+  { label: "30 min", minutes: 30 },
+  { label: "45 min", minutes: 45 },
+  { label: "1 hour", minutes: 60 },
+  { label: "1.5 hours", minutes: 90 },
+  { label: "2 hours", minutes: 120 },
+];
