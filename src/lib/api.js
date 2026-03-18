ddccb3485facd2c41295fb1412359fd549b711b3
SunnyDev7 <sunnydev4589@gmail.com>
2026-03-18 22:38:28 +0100

    vercel config file added

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.BACKEND_LOCAL_API_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_BACKEND_API_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
